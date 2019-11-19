2f816a494e8d31373b50461b38925d8ac37772b1
John Carlo M. Ramos <44812701+N00172468@users.noreply.github.com>
2019-11-19 15:12:01 +0000

    Migrated Fakers

--- database/seeds/BooksTableSeeder.php
+++ database/seeds/BooksTableSeeder.php
@@ -3,15 +3,17 @@
 # @Date:   2019-11-19T14:28:05+00:00
 # @Email:  !!!!!---CTRL + ALT + C = Colour Picker---!!!!!
 # @Last modified by:   John Carlo M. Ramos
-# @Last modified time: 2019-11-19T14:28:16+00:00
+# @Last modified time: 2019-11-19T14:41:34+00:00
 
 
 
 
 use Illuminate\Database\Seeder;
+use App\Publisher;
 
 class BooksTableSeeder extends Seeder
 {
+  private $amountOfPublishers = 0;
     /**
      * Run the database seeds.
      *
@@ -19,6 +21,12 @@ class BooksTableSeeder extends Seeder
      */
     public function run()
     {
-        
+      $this->amountOfPublishers = Publisher::count();
+
+      factory(App\Book::class, 50)->create([
+        'publisher_id' => function() {
+          return mt_rand(1, $this->amountOfPublishers);
+        }
+      ]);
     }
 }
--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -2,8 +2,8 @@
 # @Author: Mohammed Cherbatji <mo>
 # @Date:   2019-09-26T12:45:24+01:00
 # @Filename: DatabaseSeeder.php
-# @Last modified by:   mo
-# @Last modified time: 2019-11-14T13:50:01+00:00
+# @Last modified by:   John Carlo M. Ramos
+# @Last modified time: 2019-11-19T14:32:47+00:00
 
 
 
@@ -22,5 +22,6 @@ class DatabaseSeeder extends Seeder
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
         $this->call(PublishersTableSeeder::class);
+        $this->call(BooksTableSeeder::class);
     }
 }
